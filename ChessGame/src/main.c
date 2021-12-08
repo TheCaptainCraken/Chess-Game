c7e90258f823ee965fe546b79b9bb196f4ff329e
TheCaptainCraken <thecaptaincraken@gmail.com>
2021-12-08 17:26:08 +0100

    added player turns and moving system

--- ChessGame/src/main.c
+++ ChessGame/src/main.c
@@ -4,6 +4,13 @@
 #include "libraries/loader.h" 
 #include "libraries/events.h"
 
+typedef struct {
+    int x, y;
+}Move;
+
+Move* GenerateMoves(Move start, Board board, int* nm);
+bool CheckMove(Move* moves, int nm, Move move);
+
 int main(int argc, char** argv) {
     InitWindow(1000, 800, "Chess"); /* inizalize the window and the OpenGL contex */
 
@@ -22,6 +29,10 @@ int main(int argc, char** argv) {
     LoadFenString(board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); /* this is the initial state */
     PrintBoardOnTerminal(board); /* for debug porpuses */
 
+    char current_player = 'w';
+    Move* possible_moves = NULL;
+    int number_of_moves = 0;
+
     bool is_pos_selected = false;
     int selected_row = -1;
     int selected_col = -1;
@@ -33,22 +44,35 @@ int main(int argc, char** argv) {
             int row, col;
             GetBoardPosition(&row, &col);
             if (!is_pos_selected && *(*(board + row) + col) != EMPTY) {
-                is_pos_selected = true;
-                selected_col = col;
-                selected_row = row;
-
+                if (current_player == 'w' && (*(*(board + row) + col) >= 'A' && *(*(board + row) + col) <= 'Z')) {
+                    is_pos_selected = true;
+                    selected_col = col;
+                    selected_row = row;
+                    possible_moves = GenerateMoves((Move) { selected_col, selected_row }, board, & number_of_moves);
+                }
+                else if (current_player == 'b' && (*(*(board + row) + col) >= 'a' && *(*(board + row) + col) <= 'z')) {
+                    is_pos_selected = true;
+                    selected_col = col;
+                    selected_row = row;
+                    possible_moves = GenerateMoves((Move) { selected_col, selected_row }, board, & number_of_moves);
+                }
             }
             else if (is_pos_selected && row == selected_row && col == selected_col) {
                 is_pos_selected = false;
                 selected_row = -1;
                 selected_col = -1;
+                MemFree(possible_moves);
+                number_of_moves = 0;
             }
-            else if (is_pos_selected) {
+            else if (is_pos_selected && CheckMove(possible_moves, number_of_moves, (Move) { col, row })) {
                 *(*(board + row) + col) = *(*(board + selected_row) + selected_col);
                 *(*(board + selected_row) + selected_col) = EMPTY;
                 is_pos_selected = false;
                 selected_row = -1;
                 selected_col = -1;
+                MemFree(possible_moves);
+                number_of_moves = 0;
+                current_player = (current_player == 'w') ? 'b' : 'w';
             }
         }
 
@@ -62,4 +86,24 @@ int main(int argc, char** argv) {
     DestroyAssets(textures); /* deallocate the textures  */
     CloseWindow();
     return 0;
+}
+
+Move* GenerateMoves(Move start, Board board, int* nm) {
+    *nm = 1;
+    if (*(*(board + start.y) + start.x) >= 'a' && *(*(board + start.y) + start.x) <= 'z') {
+        Move* possible_moves = MemAlloc(sizeof(Move));
+        *possible_moves = (Move){ start.x, start.y + 1 };
+    }
+    else {
+        Move* possible_moves = MemAlloc(sizeof(Move));
+        *possible_moves = (Move){ start.x, start.y - 1 };
+    }
+
+}
+
+bool CheckMove(Move* moves, int nm, Move move) {
+    for (int i = 0; i < nm; i++) {
+        if ((moves + i)->x == move.x && (moves + i)->y == move.y) { return true; }
+    }
+    return false;
 }
